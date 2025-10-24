85dcefe25d2e7c177db83e9880ec8d17daaab0ba
chyun7114 <chyun7114@naver.com>
2025-10-24 23:12:21 +0900

    test: UserInputValidator 테스트 작성 - 작성중 에러 메시지 라이팅 변경

--- src/main/java/racingcar/exception/UserInputErrorCode.java
+++ src/main/java/racingcar/exception/UserInputErrorCode.java
@@ -6,7 +6,7 @@ public enum UserInputErrorCode implements BaseErrorCode<IllegalArgumentException
 
     MAX_CAR_LENGTH_ERROR("자동차 이름은 5자 이하로만 가능합니다."),
     CAR_NAME_EMPTY_ERROR("자동차 이름은 공백일 수 없습니다."),
-    COUNT_IS_NOT_NUMBER_ERROR("시도 횟수는 숫자여야 합니다."),
+    COUNT_IS_NOT_NUMBER_ERROR("시도 횟수는 숫자로 입력해야 합니다."),
     COUNT_IS_NOT_POSITIVE_NUMBER_ERROR("시도 횟수는 양수여야 합니다.");
 
     private final String message;
